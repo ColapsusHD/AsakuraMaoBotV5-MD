fa2b80d53ef39d35245954333dded50142daa214
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2025-11-24 04:13:49 -0300

    Update group-warns.js

--- plugins/group-warns.js
+++ plugins/group-warns.js
@@ -23,10 +23,10 @@ if (who === conn.user.jid) return conn.reply(m.chat, `ꕥ No puedo ponerle adver
 if (who === ownerGroup) return conn.reply(m.chat, `ꕥ No puedo darle advertencias al propietario del grupo.`, m)
 if (who === ownerBot) return conn.reply(m.chat, `ꕥ No puedo darle advertencias al propietario del bot.`, m)
 user.warn = (user.warn || 0) + 1
-await m.reply(`*@${who.split`@`[0]}* recibió una advertencia en este grupo!\nMotivo: ${motivo}\n*Advertencias: ${user.warn}/3*`, null, { mentions: [who] })
-if (user.warn >= 3) {
+await m.reply(`*@${who.split`@`[0]}* recibió una advertencia en este grupo!\nMotivo: ${motivo}\n*Advertencias: ${user.warn}/6*`, null, { mentions: [who] })
+if (user.warn >= 6) {
 user.warn = 0
-await m.reply(`❀ ¡Te lo advertí varias veces!\n*@${who.split`@`[0]}* superó las *3* advertencias, ahora será eliminado/a.`, null, { mentions: [who] })
+await m.reply(`❀ ¡Te lo advertí varias veces!\n*@${who.split`@`[0]}* superó las *6* advertencias, ahora será eliminado/a.`, null, { mentions: [who] })
 await conn.groupParticipantsUpdate(m.chat, [who], 'remove')
 }
 break
@@ -36,13 +36,13 @@ if (!who) return m.reply(`❀ Etiqueta a un usuario para quitarle las advertenci
 if (mentionedJid.includes(conn.user.jid)) return
 if (user.warn === 0) throw `ꕥ El usuario tiene 0 advertencias.`
 user.warn -= 1
-await m.reply(`${user.warn === 1 ? `*@${who.split`@`[0]}*` : `❀ *@${who.split`@`[0]}*`} Se le quitó una advertencia.\n*ADVERTENCIAS ${user.warn}/3*`, null, { mentions: [who] })
+await m.reply(`${user.warn === 1 ? `*@${who.split`@`[0]}*` : `❀ *@${who.split`@`[0]}*`} Se le quitó una advertencia.\n*ADVERTENCIAS ${user.warn}/6*`, null, { mentions: [who] })
 break
 }
 case 'listadv': case 'advlist': {
 const adv = Object.entries(global.db.data.chats[m.chat].users).filter(([_, u]) => u.warn)
 const warns = global.db.data.chats[m.chat].users.warn || 0
-const listadvs = `❀ Usuarios Advertidos\n\n*Total : ${adv.length} Usuarios*${adv.length > 0 ? '\n' + adv.map(([jid, user]) => `*●* @${jid.split`@`[0]} : *(${user.warn}/3)*`).join('\n') : ''}\n\n⚠︎ Advertencias ⇢ *${warns ? `${warns}/3` : '0/3'}*`
+const listadvs = `❀ Usuarios Advertidos\n\n*Total : ${adv.length} Usuarios*${adv.length > 0 ? '\n' + adv.map(([jid, user]) => `*●* @${jid.split`@`[0]} : *(${user.warn}/3)*`).join('\n') : ''}\n\n⚠︎ Advertencias ⇢ *${warns ? `${warns}/6` : '0/6'}*`
 await conn.sendMessage(m.chat, { image: { url: pp }, caption: listadvs, mentions: await conn.parseMention(listadvs) }, { quoted: m })
 break
 }}} catch (error) {
